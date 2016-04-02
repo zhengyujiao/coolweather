afaff17cb2a16987937ad9bf266365a89cadae4e
zhengyujiao <1044247837@qq.com>
2016-04-02 11:31:23 +0800

    新增切换城市和手动更新天气的功能

--- src/activity/ChooseAreaActivity.java
+++ src/activity/ChooseAreaActivity.java
@@ -16,7 +16,10 @@ import com.coolweather.app.R;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
@@ -31,6 +34,7 @@ public class ChooseAreaActivity extends Activity{
 	public static final int LEVEL_PROVINCE=0;
 	public static final int LEVEL_CITY=1;
 	public static final int LEVEL_COUNTY=2;
+	protected static final String WeatherActivity = null;
 	private ProgressDialog progressDialog;
 	private TextView titleText;
 	private ListView listView;
@@ -61,9 +65,23 @@ public class ChooseAreaActivity extends Activity{
 	 * 当前选中的级别
 	 */
 	private int currentLevel;
-
+/**
+ * 是否从WeatherActivity中跳转过来
+ */
+	private boolean isFromWeatherActivity;
+	
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
+		isFromWeatherActivity=getIntent().getBooleanExtra("from_weather_activity", false);
+		SharedPreferences prefs=PreferenceManager.getDefaultSharedPreferences(this);
+		//已经选择了城市且不是从WeatherActivity跳转过来，才会直接跳转到WeatherActivity
+		if(prefs.getBoolean("city_selected", false)&&!isFromWeatherActivity){
+			Intent intent=new Intent(this,WeatherActivity.class);
+			startActivity(intent);
+			finish();
+			return;
+		}
+		
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.choose_area);
 		listView=(ListView)findViewById(R.id.list_view);
@@ -83,6 +101,12 @@ public class ChooseAreaActivity extends Activity{
 				else if(currentLevel==LEVEL_CITY){
 					selectedCity=cityList.get(index);
 					queryCounties();
+				}else if(currentLevel==LEVEL_COUNTY){
+					String countyCode=countyList.get(index).getCountyCode();
+					Intent intent=new Intent(ChooseAreaActivity.this,WeatherActivity.class);
+					intent.putExtra("county_code",countyCode);
+					startActivity(intent);
+					finish();
 				}
 			}
 
@@ -237,6 +261,10 @@ public class ChooseAreaActivity extends Activity{
 		}else if(currentLevel==LEVEL_CITY){
 			queryProvinces();
 		}else{
+			if(isFromWeatherActivity){
+				Intent intent=new Intent(this,WeatherActivity.class);
+				startActivity(intent);
+			}
 			finish();
 		}
 	}	
